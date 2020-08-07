b4749453160a75d1faa8d461699ccfb748f53e3c
dasavijit0075 <dasavijit0075@gmail.com>
2020-08-08 03:21:12 +0530

    Update Project From Team-D

---
+++ chayaprakashani/src/main/java/com/example/indusnetprj/chayaprakashani/service/ChayaprakashaniService.java
@@ -0,0 +1,169 @@
+package com.example.indusnetprj.chayaprakashani.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.NonUniqueResultException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.indusnetprj.chayaprakashani.dao.ChayaPrakashaniDAO;
+import com.example.indusnetprj.chayaprakashani.dao.StudentDAO;
+import com.example.indusnetprj.chayaprakashani.dto.CourseDTO;
+import com.example.indusnetprj.chayaprakashani.dto.StudentDTO;
+import com.example.indusnetprj.chayaprakashani.dto.StudentDetailsShow;
+import com.example.indusnetprj.chayaprakashani.entity.Courses;
+import com.example.indusnetprj.chayaprakashani.entity.StudentDetails;
+import com.example.indusnetprj.chayaprakashani.exception.DuplicateEntryException;
+import com.example.indusnetprj.chayaprakashani.exception.ResourceNotFoundException;
+
+@Service
+public class ChayaprakashaniService {
+	
+	 @Autowired
+	   private StudentDAO studentrepository;
+
+		@Autowired
+		private ChayaPrakashaniDAO courseRepository;
+		
+		
+		/**** List of all courses***/
+		List<StudentDTO> listsDTO;
+	
+
+		/***Register a student in chayaprakashani Platform ***/
+		public StudentDetails saveStudent(StudentDetails studentDetails) throws NonUniqueResultException {
+		boolean existingEmail = studentrepository.existsByEmail(studentDetails.getEmail());
+		if (existingEmail) {
+
+			throw new NonUniqueResultException();
+		}
+
+		boolean existinguserId = studentrepository.existsByUserId(studentDetails.getUserId());
+		if (existinguserId) {
+			throw new DuplicateEntryException("UserId is already registered,provide another userId");
+		}
+
+		List<Courses> courseList = (List<Courses>) studentDetails.getCourses();
+		for (Courses course : courseList) {
+			Integer courseId = course.getCourseId();
+			boolean ans = courseRepository.existsByCourseId(courseId);
+			if (!ans) {
+				throw new ResourceNotFoundException("Course Not Found for course id : " + courseId);
+			}
+		}
+		StudentDetails newStudentDetails = new StudentDetails();
+		newStudentDetails.setFirstName(studentDetails.getFirstName());
+
+		newStudentDetails.setLastName(studentDetails.getLastName());
+		newStudentDetails.setUserId(studentDetails.getUserId());
+		newStudentDetails.setEmail(studentDetails.getEmail());
+		newStudentDetails.setPassword(studentDetails.getPassword());
+		newStudentDetails.setMobile(studentDetails.getMobile());
+		String role = studentDetails.getRoles();
+
+		if (role == null) {
+			newStudentDetails.setRoles("ROLE_USER");
+		}
+		newStudentDetails.getCourses().addAll(studentDetails.getCourses().stream().map(courses -> {
+			Courses course = courseRepository.findCourseByCourseId(courses.getCourseId());
+			course.getStudentDetails().add(newStudentDetails);
+			return course;
+		}).collect(Collectors.toList()));
+		return studentrepository.save(newStudentDetails);
+	}
+		
+		/*Single students details*/
+		public Optional<StudentDetailsShow> getStudentByuserId(String userId) {
+				
+			Optional<StudentDetails> studDetails=Optional.ofNullable(studentrepository.findByuserId(userId).orElseThrow(() -> new ResourceNotFoundException("Student not found:")));
+			StudentDetails stud=studDetails.get();
+ 
+
+			return Optional.of(new StudentDetailsShow(stud.getFirstName(), stud.getLastName(), stud.getEmail(), 
+					stud.getMobile(), stud.getCourses()));
+		}
+		
+		
+		/*Student found by course name*/
+		
+		public List<StudentDTO> studentsByCourse(String courseName) {		
+			boolean ans = courseRepository.existsBycourseName(courseName);
+			if(!ans) {
+				throw new ResourceNotFoundException( "Course Not Found");
+			}
+			else if (studentrepository.findByCourseNameIgnoreCase(courseName).isEmpty()) {
+				
+			 throw new ResourceNotFoundException("No student present for this specific course/ check course name ");
+						
+					}
+			return studentrepository.findByCourseNameIgnoreCase(courseName) ;	
+		}
+		
+		/*counting course*/
+		
+		public CourseDTO getCountByCourse(String courseName) {
+			boolean ans = courseRepository.existsBycourseName(courseName);
+			if(!ans) {
+				throw new ResourceNotFoundException( "Course Not Found");
+			}
+			else if (studentrepository.findByCourseNameIgnoreCase(courseName).isEmpty()) {
+				
+		 throw new ResourceNotFoundException("No student present for this specific course/ check course name ");
+							
+				}
+			return studentrepository.countByCourseNameIgnoreCase(courseName);
+		}
+
+		public List<Courses> findAllByPage(Integer pageNo, Integer pageSize, String sortBy) {
+
+
+			Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+			Page<Courses> pageRequest = courseRepository.findAll(paging); 
+			
+			if(pageRequest.hasContent())
+			{
+				return pageRequest.getContent();
+			}
+			
+			else
+			{
+				return new ArrayList<Courses>();
+			}
+			
+			
+		}
+		
+/* Admin can show all details*/
+		
+
+		public List<StudentDetails> getJoinInformation(Integer page, Integer size, String sort) {
+			
+			Pageable paging = PageRequest.of(page, size, Sort.by(sort));
+			Page<StudentDetails> pageRequest = studentrepository.getAllStudentExceptAdmin(paging); 
+			
+			if(pageRequest.hasContent())
+			{
+				return pageRequest.getContent();
+			}
+			
+			else
+			{
+				return new ArrayList<StudentDetails>();
+			}
+			
+		}
+}
+
+
+
+
+
+
